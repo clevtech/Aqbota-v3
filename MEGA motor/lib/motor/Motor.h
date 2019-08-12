cfc6872b0c85fc55bb14065c523a443525589ec8
BiggyBaron <dr.cleverest@gmail.com>
2019-08-12 16:32:13 +0600

    Deleted servo.h

--- MEGA motor/lib/motor/Motor.h
+++ MEGA motor/lib/motor/Motor.h
@@ -1,7 +1,6 @@
 #ifndef MOTOR_H
 #define MOTOR_H
 
-#include <Servo.h> 
 #include <Arduino.h>
 
 class Controller
@@ -12,7 +11,6 @@ class Controller
         void spin(int pwm);
 
     private:
-        Servo motor_;
         driver motor_driver_;
         int pwm_pin_;
         int motor_pinA_;
