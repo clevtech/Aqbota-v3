3c7b7e5a21aabe199a9ab5c05ca4d8fa0245b52b
BiggyBaron <dr.cleverest@gmail.com>
2019-08-12 18:30:25 +0600

    Try one blink

---
+++ MEGA motor/oldcode.cpp
@@ -0,0 +1,161 @@
+#if (ARDUINO >= 100)
+    #include <Arduino.h>
+#else
+    #include <WProgram.h>
+#endif
+
+#include <analogWrite.h>
+
+#include "ros.h"
+#include "ros/time.h"
+//header file for publishing velocities for odom
+#include "lino_msgs/Velocities.h"
+//header file for cmd_subscribing to "cmd_vel"
+#include "geometry_msgs/Twist.h"
+
+
+#include "lino_base_config.h"
+#include "Motor.h"
+#include "Kinematics.h"
+#include "PID.h"
+
+
+#define IMU_PUBLISH_RATE 20 //hz
+#define COMMAND_RATE 20 //hz
+#define DEBUG_RATE 5
+
+Controller motor1_controller(Controller::MOTOR_DRIVER, MOTOR1_PWM, MOTOR1_IN_A, MOTOR1_IN_B);
+Controller motor2_controller(Controller::MOTOR_DRIVER, MOTOR2_PWM, MOTOR2_IN_A, MOTOR2_IN_B); 
+Controller motor3_controller(Controller::MOTOR_DRIVER, MOTOR3_PWM, MOTOR3_IN_A, MOTOR3_IN_B);
+Controller motor4_controller(Controller::MOTOR_DRIVER, MOTOR4_PWM, MOTOR4_IN_A, MOTOR4_IN_B);
+
+PID motor1_pid(PWM_MIN, PWM_MAX, K_P, K_I, K_D);
+PID motor2_pid(PWM_MIN, PWM_MAX, K_P, K_I, K_D);
+PID motor3_pid(PWM_MIN, PWM_MAX, K_P, K_I, K_D);
+PID motor4_pid(PWM_MIN, PWM_MAX, K_P, K_I, K_D);
+
+Kinematics kinematics(Kinematics::LINO_BASE, MAX_RPM, WHEEL_DIAMETER, FR_WHEELS_DISTANCE, LR_WHEELS_DISTANCE);
+
+float g_req_linear_vel_x = 0;
+float g_req_linear_vel_y = 0;
+float g_req_angular_vel_z = 0;
+
+unsigned long g_prev_command_time = 0;
+
+//callback function prototypes
+void commandCallback(const geometry_msgs::Twist& cmd_msg);
+
+ros::NodeHandle nh;
+
+ros::Subscriber<geometry_msgs::Twist> cmd_sub("cmd_vel", commandCallback);
+
+lino_msgs::Velocities raw_vel_msg;
+ros::Publisher raw_vel_pub("raw_vel", &raw_vel_msg);
+
+
+void printDebug();
+void moveBase();
+void stopBase();
+
+
+void setup()
+{    
+    nh.initNode();
+    nh.getHardware()->setBaud(57600);
+    nh.subscribe(cmd_sub);
+    nh.advertise(raw_vel_pub);
+
+    while (!nh.connected())
+    {
+        nh.spinOnce();
+    }
+    nh.loginfo("MOTOR CONTROLLER CONNECTED");
+    delay(1);
+}
+
+void loop()
+{
+    static unsigned long prev_control_time = 0;
+    static unsigned long prev_debug_time = 0;
+
+    //this block drives the robot based on defined rate
+    if ((millis() - prev_control_time) >= (1000 / COMMAND_RATE))
+    {
+        moveBase();
+        prev_control_time = millis();
+    }
+
+    //this block stops the motor when no command is received
+    if ((millis() - g_prev_command_time) >= 400 or !Serial.available())
+    {
+        stopBase();
+    }
+
+    //this block displays the encoder readings. change DEBUG to 0 if you don't want to display
+    if(DEBUG)
+    {
+        if ((millis() - prev_debug_time) >= (100000 / DEBUG_RATE))
+        {
+            printDebug();
+            prev_debug_time = millis();
+        }
+    }
+    //call all the callbacks waiting to be called
+    nh.spinOnce();
+}
+
+void commandCallback(const geometry_msgs::Twist& cmd_msg)
+{
+    //callback function every time linear and angular speed is received from 'cmd_vel' topic
+    //this callback function receives cmd_msg object where linear and angular speed are stored
+    g_req_linear_vel_x = cmd_msg.linear.x;
+    g_req_linear_vel_y = cmd_msg.linear.y;
+    g_req_angular_vel_z = cmd_msg.angular.z;
+
+    g_prev_command_time = millis();
+}
+
+void moveBase()
+{
+    //get the required rpm for each motor based on required velocities, and base used
+    Kinematics::rpm req_rpm = kinematics.getRPM(g_req_linear_vel_x, g_req_linear_vel_y, g_req_angular_vel_z);
+
+    //get the current speed of each motor
+    int current_rpm1 = 0;
+    int current_rpm2 = 0;
+
+    //the required rpm is capped at -/+ MAX_RPM to prevent the PID from having too much error
+    //the PWM value sent to the motor driver is the calculated PID based on required RPM vs measured RPM
+    motor1_controller.spin(motor1_pid.compute(req_rpm.motor1, current_rpm1));
+    motor2_controller.spin(motor2_pid.compute(req_rpm.motor2, current_rpm2));   
+
+    Kinematics::velocities current_vel;
+
+    current_vel = kinematics.getVelocities(current_rpm1, current_rpm2, 0, 0);
+    
+    //pass velocities to publisher object
+    raw_vel_msg.linear_x = current_vel.linear_x;
+    raw_vel_msg.linear_y = current_vel.linear_y;
+    raw_vel_msg.angular_z = current_vel.angular_z;
+
+    //publish raw_vel_msg
+    raw_vel_pub.publish(&raw_vel_msg);
+}
+
+void stopBase()
+{
+    g_req_linear_vel_x = 0;
+    g_req_linear_vel_y = 0;
+    g_req_angular_vel_z = 0;
+}
+
+void printDebug()
+{
+    char buffer[50];
+
+    sprintf (buffer, "Encoder FrontLeft  : %f", 0);
+    nh.loginfo(buffer);
+    sprintf (buffer, "Encoder FrontRight : %f", 0);
+    nh.loginfo(buffer);
+}
+
