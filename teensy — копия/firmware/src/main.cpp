b3cff5ec6c9dbc88688df720bb621b37f45490cb
BiggyBaron <dr.cleverest@gmail.com>
2019-08-12 16:22:26 +0600

    New mega drive

--- teensy — копия/firmware/src/main.cpp
+++ teensy — копия/firmware/src/main.cpp
@@ -16,23 +16,23 @@
 #define COMMAND_RATE 20 //hz
 #define DEBUG_RATE 5
 
-const char* ssid     = "Friends";
-const char* password = "The2ndl@w";
+const char* ssid     = "aqbotain";
+const char* password = "The2ndlaw";
 // Set the rosserial socket server IP address
-IPAddress server(192,168,1,149);
+IPAddress server(10,42,0,1);
 // Set the rosserial socket server port
 const uint16_t serverPort = 11411;
 
 
 // CONSTANTS
 
-int MOTOR_A_FWD = 13;
-int MOTOR_A_BCW = 12;
+int MOTOR_A_FWD = 12;
+int MOTOR_A_BCW = 13;
 int MOTOR_B_FWD = 14;
 int MOTOR_B_BCW = 27;
 
-int L_ENC = 34;
-int R_ENC = 35;
+// int L_ENC = 34;
+// int R_ENC = 35;
 
 
 float g_req_linear_vel_x = 0;
@@ -43,8 +43,8 @@ unsigned long prev_control_time = 0;
 unsigned long prev_debug_time = 0;
 
 
-float last_dir1 = 1;
-float last_dir2 = 1;
+// float last_dir1 = 1;
+// float last_dir2 = 1;
 
 unsigned long g_prev_command_time = 0;
 
@@ -55,50 +55,50 @@ ros::NodeHandle nh;
 ros::Subscriber<geometry_msgs::Twist> cmd_sub("cmd_vel", commandCallback);
 
 
-lino_msgs::Velocities raw_vel_msg;
-ros::Publisher raw_vel_pub("raw_vel", &raw_vel_msg);
+// lino_msgs::Velocities raw_vel_msg;
+// ros::Publisher raw_vel_pub("raw_vel", &raw_vel_msg);
 
 void moveBase();
 void stopBase();
 void printDebug();
-void getRPM();
+// void getRPM();
 
 // Encoder part
-volatile int interruptCounterL = 0;
-int numberOfInterruptsL = 0;
-int lastL = millis();
-int deltaL = 0;
-int nowL = 0;
-double RPML = 0;
-int echoL = 0;
-volatile int interruptCounterR = 0;
-int numberOfInterruptsR = 0;
-int lastR = millis();
-int deltaR = 0;
-int nowR = 0;
-double RPMR = 0;
-int echoR = 0;
+// volatile int interruptCounterL = 0;
+// int numberOfInterruptsL = 0;
+// int lastL = millis();
+// int deltaL = 0;
+// int nowL = 0;
+// double RPML = 0;
+// int echoL = 0;
+// volatile int interruptCounterR = 0;
+// int numberOfInterruptsR = 0;
+// int lastR = millis();
+// int deltaR = 0;
+// int nowR = 0;
+// double RPMR = 0;
+// int echoR = 0;
 
 
-portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;
+// portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;
 
-void IRAM_ATTR leftEncoder() {
-  portENTER_CRITICAL_ISR(&mux);
-  interruptCounterL = interruptCounterL + 1;
-  portEXIT_CRITICAL_ISR(&mux);
-}
+// void IRAM_ATTR leftEncoder() {
+//   portENTER_CRITICAL_ISR(&mux);
+//   interruptCounterL = interruptCounterL + 1;
+//   portEXIT_CRITICAL_ISR(&mux);
+// }
 
-void IRAM_ATTR rightEncoder() {
-  portENTER_CRITICAL_ISR(&mux);
-  interruptCounterR = interruptCounterR + 1;
-  portEXIT_CRITICAL_ISR(&mux);
-}
+// void IRAM_ATTR rightEncoder() {
+//   portENTER_CRITICAL_ISR(&mux);
+//   interruptCounterR = interruptCounterR + 1;
+//   portEXIT_CRITICAL_ISR(&mux);
+// }
 
 
 void setup()
 {   
-    pinMode(L_ENC, INPUT_PULLUP);
-    pinMode(R_ENC, INPUT_PULLUP);
+    // pinMode(L_ENC, INPUT_PULLUP);
+    // pinMode(R_ENC, INPUT_PULLUP);
     pinMode(MOTOR_A_FWD, OUTPUT);
     pinMode(MOTOR_B_FWD, OUTPUT);
     pinMode(MOTOR_A_BCW, OUTPUT);
@@ -137,13 +137,12 @@ void setup()
     // attachInterrupt(digitalPinToInterrupt(L_ENC), leftEncoder, RISING);
     // attachInterrupt(digitalPinToInterrupt(R_ENC), rightEncoder, RISING);
     Serial.println("CONNECTED...");
-    nh.loginfo("LINOBASE CONNECTED");
+    nh.loginfo("MOTOR CONTROLLER CONNECTED");
     
 }
 
 void loop()
 {
-    Serial.println("CONNECTED!");
 
     //this block drives the robot based on defined rate
     if ((millis() - prev_control_time) >= (100))
@@ -156,19 +155,24 @@ void loop()
     if ((millis() - g_prev_command_time) >= 400)
     {
         stopBase();
+    }
+    if (!nh.connected())
+    {
+        stopBase();
         moveBase();
+        ESP.restart();
     }
 
 
     //this block displays the encoder readings. change DEBUG to 0 if you don't want to display
-    if(DEBUG)
-    {
-        if ((millis() - prev_debug_time) >= (2000))
-        {
-            printDebug();
-            prev_debug_time = millis();
-        }
-    }
+    // if(DEBUG)
+    // {
+    //     if ((millis() - prev_debug_time) >= (2000))
+    //     {
+    //         printDebug();
+    //         prev_debug_time = millis();
+    //     }
+    // }
     //call all the callbacks waiting to be called
     nh.spinOnce();
 }
@@ -188,46 +192,49 @@ void moveBase()
     int pwmL = 0;
     int dirR = 0;
     int dirL = 0;
+
     
-    pwmR = (g_req_linear_vel_x - g_req_angular_vel_z/2)*255;
-    pwmL = (g_req_linear_vel_x + g_req_angular_vel_z/2)*255;
+    
+    pwmR = (g_req_linear_vel_x - g_req_angular_vel_z/2)*200;
+    pwmL = (g_req_linear_vel_x + g_req_angular_vel_z/2)*200;
+    pwmR = constrain(pwmR, -200, 200);
+    pwmL = constrain(pwmL, -200, 200);
 
-    pwmR = constrain(pwmR, -255, 255);
-    pwmL = constrain(pwmL, -255, 255);
+    // Serial.print("pwmR is: ");
+    // Serial.println(pwmR);
 
-    if(pwmL>0){
+    // Serial.print("pwmL is: ");
+    // Serial.println(pwmL);
+
+    if(pwmR>=0){
         analogWrite(MOTOR_A_BCW, 0);
-        analogWrite(MOTOR_A_FWD, abs(pwmL));
-        dirL = 1;
+        analogWrite(MOTOR_A_FWD, pwmR);
     }
     else{
         analogWrite(MOTOR_A_FWD, 0);
-        analogWrite(MOTOR_A_BCW, abs(pwmL));
-        dirL = -1;
+        analogWrite(MOTOR_A_BCW, -pwmR);
     }
-    if(pwmR>0){
+    if(pwmL>=0){
         analogWrite(MOTOR_B_BCW, 0);
-        analogWrite(MOTOR_B_FWD, abs(pwmR));
-        dirR = 1;
+        analogWrite(MOTOR_B_FWD, pwmL);
     }
     else{
         analogWrite(MOTOR_B_FWD, 0);
-        analogWrite(MOTOR_B_BCW, abs(pwmR));
-        dirR = -1;
+        analogWrite(MOTOR_B_BCW, -pwmL);
     }
 
-    getRPM();
+    // getRPM();
 
-    //convert average revolutions per minute to revolutions per second
-    float average_rps_x = ((float)(dirL*RPML + dirR*RPMR) / 2) / 60; // RPM
-    raw_vel_msg.linear_x = average_rps_x * 0.54; // m/s
-    raw_vel_msg.linear_y = 0;
+    // //convert average revolutions per minute to revolutions per second
+    // float average_rps_x = ((float)(dirL*RPML + dirR*RPMR) / 2) / 60; // RPM
+    // raw_vel_msg.linear_x = average_rps_x * 0.54; // m/s
+    // raw_vel_msg.linear_y = 0;
 
-    //convert average revolutions per minute to revolutions per second
-    float average_rps_a = ((float)(-dirL*RPML + dirR*RPMR) / 2) / 60;
-    raw_vel_msg.angular_z =  (average_rps_a * 0.54) / (0.25 / 2); //  rad/s
+    // //convert average revolutions per minute to revolutions per second
+    // float average_rps_a = ((float)(-dirL*RPML + dirR*RPMR) / 2) / 60;
+    // raw_vel_msg.angular_z =  (average_rps_a * 0.54) / (0.25 / 2); //  rad/s
 
-    raw_vel_pub.publish(&raw_vel_msg);
+    // raw_vel_pub.publish(&raw_vel_msg);
 }
 
 void stopBase()
@@ -238,30 +245,30 @@ void stopBase()
 }
 
 
-void printDebug()
-{
-    getRPM();
-    char buffer[50];
-    sprintf (buffer, "RPM left  : %f", RPML);
-    nh.loginfo(buffer);
-    sprintf (buffer, "RPM right : %f", RPMR);
-    nh.loginfo(buffer);
-}
+// void printDebug()
+// {
+//     // getRPM();
+//     char buffer[50];
+//     sprintf (buffer, "RPM left  : %f", RPML);
+//     nh.loginfo(buffer);
+//     sprintf (buffer, "RPM right : %f", RPMR);
+//     nh.loginfo(buffer);
+// }
 
-void getRPM(){
-    echoL = millis();
-    echoR = echoL;
-    portENTER_CRITICAL(&mux);
-    numberOfInterruptsL = interruptCounterL;
-    interruptCounterL = 0;
-    nowL = millis();
-    deltaL = nowL - lastL;
-    lastL = nowL;
-    numberOfInterruptsR = interruptCounterR;
-    interruptCounterR = 0;
-    deltaR = nowL - lastR;
-    lastR = nowR;
-    portEXIT_CRITICAL(&mux);
-    RPML = (numberOfInterruptsL*60000)/(2*8*deltaL);
-    RPMR = (numberOfInterruptsR*60000)/(2*8*deltaR);
-}
+// void getRPM(){
+//     echoL = millis();
+//     echoR = echoL;
+//     portENTER_CRITICAL(&mux);
+//     numberOfInterruptsL = interruptCounterL;
+//     interruptCounterL = 0;
+//     nowL = millis();
+//     deltaL = nowL - lastL;
+//     lastL = nowL;
+//     numberOfInterruptsR = interruptCounterR;
+//     interruptCounterR = 0;
+//     deltaR = nowL - lastR;
+//     lastR = nowR;
+//     portEXIT_CRITICAL(&mux);
+//     RPML = (numberOfInterruptsL*60000)/(2*8*deltaL);
+//     RPMR = (numberOfInterruptsR*60000)/(2*8*deltaR);
+// }
